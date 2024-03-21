70dd2bf7095a890d6c17883b9f975bcfb8bd7aa3
traineeEngineer <hitlersuri3@gmail.com>
2024-03-21 12:20:43 +0530

    Update Csv_tool.py

--- DataAnlysis_tool/Csv_tool.py
+++ DataAnlysis_tool/Csv_tool.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from dotenv import load_dotenv
+from utils import query_agent
 
+load_dotenv()
+
+
+st.title("Let's do some analysis on your csv")
+st.header('please upload your csv file here')
+
+data=st.file_uploader("upload csv file",type='csv')
+
+query=st.text_area("Enter your query")
+button=st.button('Generate Response')
+
+if button:
+    answer= query_agent(data,query)
+    st.write(answer)
