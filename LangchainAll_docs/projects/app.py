7725b25c79935411ec81dcb0f76ac54322fca311
traineeEngineer <hitlersuri3@gmail.com>
2024-10-10 10:23:43 +0530

    multi-doc-app

--- LangchainAll_docs/projects/app.py
+++ LangchainAll_docs/projects/app.py
@@ -1,53 +1,30 @@
 import os
-import streamlit as st 
-from helpers import *
+from dotenv import find_dotenv, load_dotenv
+import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.chains.question_answering import load_qa_chain
 
 
+load_dotenv(find_dotenv())
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+#==== Using OpenAI Chat API =======
+llm_model = "gpt-3.5-turbo"
+llm = ChatOpenAI(temperature=0.0, model=llm_model)
+
+#### === packages to install ====
+# pip install langchain pypdf openai chromadb tiktoken docx2txt
+
+# load the pdf file
+pf_loader = PyPDFLoader('./docs/RachelGreenCV.pdf')
+documents = pf_loader.load()
 
 
-def main():
-    st.set_page_config(page_title="Researcher...", 
-                       page_icon=":parrot:", 
-                       layout="wide")
-    
-    st.header("Generate a Newsletter :parrot:")
-    query = st.text_input("Enter a topic...")
-    
-    if query:
-        print(query)
-        st.write(query)
-        with st.spinner(f"Generating newsletter for {query}"):
-            #st.write("Generating newsletter for: ", query)
-            
-            search_results = search_serp(query=query)
-            urls = pick_best_articles_urls(response_json=search_results, query=query)
-            data = extract_content_from_urls(urls)
-            summaries = summarizer(data, query)
-            newsletter_thread = generate_newsletter(summaries, query)
-            
-            with st.expander("Search Results"):
-                st.info(search_results)
-            with st.expander("Best URLs"):
-                st.info(urls)
-            with st.expander("Data"):
-                # iterate through data in the FAISS db and return the similiarity search data to show!
-                data_raw = " ".join(d.page_content for d in data.similarity_search(query,k=4))
-                st.info(data_raw)
-            with st.expander("Summaries"):
-                st.info(summaries)
-            with st.expander("Newsletter:"):
-                st.info(newsletter_thread)
-        st.success("Done!")
-    # query = "Flutter development news"
-    # resp = search_serp(query=query)
-    # urls = pick_best_articles_urls(response_json=resp, query=query)
-    # data = extract_content_from_urls(urls=urls)
-    # summaries = summarizer(db=data, query=query)
-    # newsletter = generate_newsletter(summaries=summaries, query=query)
-    # print(newsletter)
+#set up qa chain
+chain = load_qa_chain(llm, verbose=True)
+query = 'Where did Rachel go to school?'
+response = chain.run(input_documents=documents,
+                     question=query)
 
-
-
-#Invoking main function
-if __name__ == '__main__':
-    main()
+print(response)
